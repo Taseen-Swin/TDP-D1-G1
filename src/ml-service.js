7521a23b7b41eae1da068dc4b95fb7c04293ee39
Taseen <taseen@sofy.ai>
2024-10-28 20:34:25 +1100

    commit

--- src/ml-service.js
+++ src/ml-service.js
@@ -8,18 +8,18 @@ class MLService {
     }
 
     async makeFuturePrediction() {
-        // let options = {
-        //     scriptPath: '', // Adjust to where your Python script is located
-        //     args: [] // You can pass any arguments here if needed
-        //   };
+        let options = {
+            scriptPath: '', // Adjust to where your Python script is located
+            args: [] // You can pass any arguments here if needed
+          };
         
-        //   // Run the Python script for prediction
-        //   PythonShell.run('predict_future.py', options, (err, results) => {
-        //     if (err) throw err;
-        //     // Parse the predictions returned by the Python script
-        //     let predictions = JSON.parse(results[0]);
-        //     res.json(predictions);
-        //   });
+          // Run the Python script for prediction
+          PythonShell.run('predict_future.py', options, (err, results) => {
+            if (err) throw err;
+            // Parse the predictions returned by the Python script
+            let predictions = JSON.parse(results[0]);
+            res.json(predictions);
+          });
     }
 }
 
